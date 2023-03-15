dcf8cc6a0a87f751b71c965ab7de0e4da99d5aef
devoremcintosh <devoremcintosh@gmail.com>
2023-03-15 00:11:55 -0400

    first commit

---
+++ shapes/src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+import { shapes } from './Shapes';
+import React from 'react';
+
+const title ="";
+const images = [];
+
+for(const shape in shapes) {
+  images.push(
+    <img 
+       key={shape} 
+       className = 'shape'
+       alt={shapes[shape].name}
+       src={shapes[shape].image}
+       role='button'
+       onClick={displayName}
+    />
+  )
+}
+
+function displayName(e) {
+  const selectedShape = e.target.alt;
+  const shapeInfo = shapes[selectedShape];
+  
+  const name = shapeInfo.name;
+
+  document.getElementById('name').innerHTML = name;
+
+}
+
+function App() {
+  return (
+    <div className="App">
+      <h1>{ title === "" ? "Click on a shape for the name" : title }</h1>
+      <p id='name'></p>
+      <div className='shapes'>
+        {images}
+     </div>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ shapes/src/Shapes.js
@@ -0,0 +1,14 @@
+export const shapes = {
+    square: {
+        name: 'square',
+        image: '/images/square.png'
+    },
+    circle: {
+        name: 'circle',
+        image: '/images/circle.png'
+    },
+    triangle: {
+        name: 'triangle',
+        image: '/images/triangle.png'
+    }
+};
